95d0fc342103c286db0d7821cff8c90ed68c654e
amin ahmed <aminahmed24@gmail.com>
2023-11-28 01:49:11 +0200

    task 1 FIX handle null text pointer

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,13 +21,11 @@ int create_file(const char *filename, char *text_content)
 	if (fo < 0)
 		return (-1);
 
-	while (text_content[c])
+	while (text_content && text_content[c])
 		c++;
 
 	if (write(fo, text_content, c) < c)
-	{
 		ret = -1;
-	}
 
 	close(fo);
 
