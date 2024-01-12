5f32bdb384624061472f3c676249a91435fb5e94
amin ahmed <aminahmed24@gmail.com>
2024-01-12 21:56:06 +0200

    7. insert at index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* insert_dnodeint_at_index - insert a note at index
+* @h: head pointer
+* @idx: the index
+* @n: the value of the node
+* Return: the new node
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current = *h;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	unsigned int i;
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	for (i = 0; i < idx && current; i++, current = current->next)
+		;
+	if (i != idx)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->prev = current->prev;
+	new->next = current;
+	current->prev = new;
+	if (*h == NULL)
+		*h = new;
+
+	return (new);
+}
