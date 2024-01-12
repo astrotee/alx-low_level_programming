f3ca44d41b7fb1087e17709a877d3971d6adc1a8
amin ahmed <aminahmed24@gmail.com>
2024-01-12 21:38:14 +0200

    6. sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* sum_dlistint - calculate the sum of dlist
+* @head: head pointer
+* Return: the sum
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int sum = 0;
+
+	while (current && current->next)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
