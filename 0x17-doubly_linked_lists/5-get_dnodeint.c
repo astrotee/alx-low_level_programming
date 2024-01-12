2c4af896d87b676ce2ab111a7256ccca454d2a1f
amin ahmed <aminahmed24@gmail.com>
2024-01-12 21:26:28 +0200

    use unsigned int

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current = head;
-	int i = 0;
+	unsigned int i = 0;
 
 	for (current = head; current && i < index; current = current->next, i++)
 		;
