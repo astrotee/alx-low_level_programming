06d4063e35e71833f9b6f976912ffa335232cd47
amin ahmed <aminahmed24@gmail.com>
2024-02-02 20:34:15 +0200

    allocate array of pointers correctly

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,25 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	hash_node_t *nodes_array;
+	hash_node_t **nodes_array;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	nodes_array = malloc(sizeof(hash_node_t) * size);
-	if (size && nodes_array == NULL)
+	nodes_array = malloc(sizeof(hash_node_t *));
+	if (nodes_array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	new_table->array = &nodes_array;
+	*nodes_array = malloc(sizeof(hash_node_t *) * size);
+	if (size && *nodes_array == NULL)
+	{
+		free(new_table);
+		free(nodes_array);
+		return (NULL);
+	}
+	new_table->array = nodes_array;
 	return (new_table);
 }
