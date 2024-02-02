adc31a7d294225787547766f9a5741002c9ccd5e
amin ahmed <aminahmed24@gmail.com>
2024-02-02 22:40:31 +0200

    refactor hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,25 +9,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	hash_node_t **nodes_array;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	nodes_array = malloc(sizeof(hash_node_t *));
-	if (nodes_array == NULL)
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (size && new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	*nodes_array = malloc(sizeof(hash_node_t *) * size);
-	if (size && *nodes_array == NULL)
-	{
-		free(new_table);
-		free(nodes_array);
-		return (NULL);
-	}
-	new_table->array = nodes_array;
 	return (new_table);
 }
