ba544efa4f38e9b32b83a5df4cdc4125eaa91a76
amin ahmed <aminahmed24@gmail.com>
2024-03-20 22:26:37 +0200

    fix right update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,11 +13,12 @@ def island_perimeter(grid):
     for i, r in enumerate(grid):
         if not isinstance(r, list):
             return 0
+        width = len(r)
         landi = 0
         bottom = i+1 if i+1 < height else -1
         while r[landi+1:].count(1) and (landi := r.index(1, landi+1)) != -1:
             left = landi - 1 if landi - 1 >= 0 else -1
-            right = landi + 1 if landi + 1 < height else -1
+            right = landi + 1 if landi + 1 < width else -1
             if left == -1 or r[left] == 0:
                 perimeter += 1
             if right == -1 or r[right] == 0:
