fcb2947bc9a6aea4069ac6cd81a2fd2bea827f8d
amin ahmed <aminahmed24@gmail.com>
2024-03-20 21:34:29 +0200

    5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""island perimeter"""
+
+
+def island_perimeter(grid):
+    """calculate the perimeter of an island"""
+    perimeter = 0
+    height = len(grid)
+    top, bottom, left, right = -1, -1, -1, -1
+
+    for i, r in enumerate(grid):
+        landi = 0
+        bottom = i+1 if i+1 < height else -1
+        while r[landi+1:].count(1) and (landi := r.index(1, landi+1)) != -1:
+            left = landi - 1 if landi - 1 >= 0 else -1
+            right = landi + 1 if landi + 1 < height else -1
+            if left != -1 and r[left] == 0:
+                perimeter += 1
+            if right != -1 and r[right] == 0:
+                perimeter += 1
+            if top != -1 and grid[top][landi] == 0:
+                perimeter += 1
+            if bottom != -1 and grid[bottom][landi] == 0:
+                perimeter += 1
+        top = i
+    return perimeter
