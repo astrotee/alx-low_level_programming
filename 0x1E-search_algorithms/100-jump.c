f6e88cf8609eaef6ddea349a8d774d3d18518c52
amin ahmed <aminahmed24@gmail.com>
2024-05-09 06:32:43 +0300

    7. Jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,54 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * min_int - get the minimum of two ints
+ * @a: the first int
+ * @b: the second int
+ * Return: the minimum of the two
+ */
+int min_int(int a, int b)
+{
+	return ((b > a) ?  a : b);
+}
+
+/**
+* jump_search - do a jump search on array
+* @array: a pointer to the array
+* @size: the size of the array
+* @value: the value to search for
+* Return: the index or -1 if not found
+*/
+int jump_search(int *array, size_t size, int value)
+{
+	int a = 0, b = 1, e = min_int(b, (int) size) - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	for (; array[e] < value; b += sqrt(size), e = min_int(b, (int) size) - 1)
+	{
+		printf("Value checked array[%d] = [%d]\n", e, array[e]);
+		a = b - 1;
+		if (a + 1 >= (int) size)
+		{
+			b += sqrt(size);
+			break;
+		}
+	}
+
+	printf("Value found between indexes [%d] and [%d]\n", a, b - 1);
+
+	for (; array[a] < value; a++, e = min_int(b, (int) size) - 1)
+	{
+		printf("Value checked array[%d] = [%d]\n", a, array[a]);
+		if (a == e)
+			return (-1);
+	}
+
+	if (array[a] == value)
+		return (a);
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,11 @@
 #define MAIN_H__
 #include <stdlib.h>
 
+
+int min_int(int a, int b);
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_divide(int *array, size_t start, size_t end, int value);
+int jump_search(int *array, size_t size, int value);
 #endif
